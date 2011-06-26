0604c111a408b11d978883b3c7a544ec9a0cfced
Jason Berry <uniqueberry@gmail.com>
2011-06-26 15:31:13 -0400

    setup pegasus with mysql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Pegasus::Application.config.secret_token = '3dd48c3ba1ca2edb5396283d6a9f2124e79fe668643c5a032c2b134c3abe88c26657733074176cfeb6de6f406e634976c161b082fdc90b81296d8cff8227fe03'
+Pegasus::Application.config.secret_token = 'b5a104019ab560d87f324ea8873bda96542d093ae8c172b3d971b0c6a3e341a492fe903badb08bcc5d79fe9a0c38a2d4de88860efa8f03807782749358e7c82c'
