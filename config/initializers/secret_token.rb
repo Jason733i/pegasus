7c74ec64ae4c9bbce974e0201142c139fad985ef
Jason Berry <uniqueberry@gmail.com>
2011-06-26 15:25:30 -0400

    pegasus project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pegasus::Application.config.secret_token = '3dd48c3ba1ca2edb5396283d6a9f2124e79fe668643c5a032c2b134c3abe88c26657733074176cfeb6de6f406e634976c161b082fdc90b81296d8cff8227fe03'
